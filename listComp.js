3298ea6d75eeb6e2fcff5cf0cdeedcfd5b9d053f
AbeMoe <40703430+AbeMoe@users.noreply.github.com>
2018-07-24 05:54:44 -0700

    Create listComp.js
    This is a short Javascript file I coded up to give users access to a function similar to that of a Python list comprehension. It will create Lists of all the various combinations of items of all the lists you give it. It will not modify the data inside of them in any way. If you are looking to do that consider using javascript .map function!

---
+++ listComp.js
@@ -0,0 +1,50 @@
+
+
+//This is all test data, you are free to delete/alter it.
+list1 = ['yes','no','maybe'];
+list2 = ['1','2','3'];
+list3 = ['x','y','z','a','b','c'];
+listHolder = [list1,list2,list3];
+
+
+//All you need to do is hand in a list of lists, the parentList
+//is there for recursion purposes,so a proper call of this should look like
+//compList(listOfLists) and that is all it needs.
+function compList(lists,parentList) {
+    if(parentList === undefined)
+    {
+        //this is our basecase
+        parentList = lists.pop();
+    }
+    childList = lists.pop();
+    //Clean out returnList to prevent it from holding the stuff from last time
+    returnList = [];
+    //For each elelment in our childlist...
+    for(var elem1 in childList)
+    {
+        for(var elem2 in parentList)
+        {
+            //...Append to a parent
+            newList = [parentList[elem2],childList[elem1]];
+
+            returnList.push(newList);
+        }
+    }
+    //By the end of this for loop each parent should have a copy of a child linked to it
+    if(lists.length === 0)
+    {
+        //if our list is empty
+        return returnList;
+    }
+    //Recursive case
+    else
+        return compList(lists,null,returnList);
+}
+
+
+somevar = compList(listHolder,null);
+
+for(var x in somevar)
+    print(somevar[x]);
+
+
