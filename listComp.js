3d6869614ef80f3853398b462f738323a25eacc0
AbeMoe <40703430+AbeMoe@users.noreply.github.com>
2018-07-29 20:41:48 -0700

    Bug fix for unnecessary array nesting
    instead of providing list comprehensions in this form [0,1,2,3,4] there was a bug for more than two arrays in which behavior like this would occur [0,1,[0,1,2]]. The contents were correct but accessing them was very annoying.

--- listComp.js
+++ listComp.js
@@ -6,41 +6,44 @@ list2 = ['1','2','3'];
 list3 = ['x','y','z','a','b','c'];
 listHolder = [list1,list2,list3];
 
-
-//All you need to do is hand in a list of lists, the parentList
-//is there for recursion purposes,so a proper call of this should look like
-//compList(listOfLists) and that is all it needs.
-function compList(lists,parentList) {
-    if(parentList === undefined)
-    {
-        //this is our basecase
-        parentList = lists.pop();
-    }
-    childList = lists.pop();
-    //Clean out returnList to prevent it from holding the stuff from last time
-    returnList = [];
-    //For each elelment in our childlist...
-    for(var elem1 in childList)
-    {
-        for(var elem2 in parentList)
-        {
-            //...Append to a parent
-            newList = [parentList[elem2],childList[elem1]];
-
-            returnList.push(newList);
-        }
-    }
-    //By the end of this for loop each parent should have a copy of a child linked to it
-    if(lists.length === 0)
-    {
-        //if our list is empty
-        return returnList;
-    }
-    //Recursive case
-    else
-        return compList(lists,returnList);
-}
-
+function compList(lists,createdLists) 
+	{
+		if(createdLists === undefined)
+		{
+			//0..this is our basecase,pop a list and..1
+			createdLists = []
+			listHolder = lists.pop();
+			//1..for each item in the list add it, as its own list to createdLists for later..2
+			for (items in listHolder)
+			{
+				listHolder[items] = [listHolder[items]]
+				createdLists.push(listHolder[items])
+			}			
+		}
+		//2..Populate the incomingList and create a new returnList..3
+		var incomingList = lists.pop();
+		var returnList = [];
+		//3..For each "fresh" element..4
+		for(var newElements in incomingList)
+		{
+			//4..Iterate through the lists we already made..5
+			for(var list in createdLists)
+			{
+				//6..copy them using slice add the new item and add that to our return list!..7
+				var newList = createdLists[list].slice()
+				newList.push(incomingList[newElements])
+				returnList.push(newList);
+			}
+		}
+		
+		if(lists.length === 0)
+		{
+			return returnList;
+		}
+		//7..Recursive case, call the function again with all the lists we've already made and..2
+		else
+			return compList(lists,returnList);
+	}	
 
 somevar = compList(listHolder);
 
